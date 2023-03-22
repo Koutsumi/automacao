d123c78ad9f915e9a9373782f578b7c9d70f9525
Koutsumi <febaccarini@gmail.com>
2023-03-22 15:01:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Automação de testes
+Projeto de automação WEB com Ruby
+
+## Após instalar o ruby
+    sudo apt-get install ruby-dev zlib1g-dev liblzma-dev
+    sudo gem install rails
+    sudo gem install bundler 
+
+## Comandos para iniciar o projeto
+    sudo gem install cucumber
+    cucumber --init
+
+## Como executar o projeto
+ - Baixar o projeto do repositório
+ - Instalar as dependencias do projeto utilizando o comando: ```sudo blundler install```
---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+require 'capybara/cucumber'
+require 'capybara/rspec'
+require 'selenium-webdriver'
+require 'prisma'
+require 'pry'
+
+ENVIRONMENT = ENV['ENVIRONMENT']
+puts "Ambiente >> #{ENVIRONMENT}"
+
+CONFIG  = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yml")
+
+Capybara.configure do |config|
+
+    config.default_driver = :selenium_chrome
+    config.app_host = CONFIG['url_default']
+    config.default_max_wait_time = 10 # espera 10s para o elemento aparecer na tela
+
+end
